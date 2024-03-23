ed693671290246627d2825313cf9e34ec5068250
mitrank <mitrankshah@yahoo.com>
2024-03-23 19:07:29 +0530

    introduced react-player to play the self video on call btn click

--- client/src/screens/RoomScreen.js
+++ client/src/screens/RoomScreen.js
@@ -1,13 +1,23 @@
 import React, { useCallback, useEffect, useState } from "react";
 import { useSocket } from "../context/SocketProvider";
+import ReactPlayer from "react-player";
 
 const RoomScreen = () => {
   const [remoteSocketid, setRemoteSocketid] = useState(null);
+  const [localStream, setLocalStream] = useState(null);
   const socket = useSocket();
 
   const handleUserJoin = useCallback(({ emailId, id }) => {
     console.log(`Email ${emailId} has joined!`);
     setRemoteSocketid(id);
+  }, []);
+
+  const handleCallUser = useCallback(async () => {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: true,
+    });
+    setLocalStream(stream);
   }, []);
 
   useEffect(() => {
@@ -23,6 +33,11 @@ const RoomScreen = () => {
     <>
       <h1>Room Screen</h1>
       <h4>{remoteSocketid ? "Connected!" : "No one in the room..."}</h4>
+      {remoteSocketid && <button onClick={handleCallUser}>Call</button>}
+      {localStream && <>
+        <h2>My Stream</h2>
+        <ReactPlayer url={localStream} playing muted width={150} height={100} />
+      </>}
     </>
   );
 };
