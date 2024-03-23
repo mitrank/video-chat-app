a7c5d459045994e66db6786b41ebafac9ab45c63
mitrank <mitrankshah@yahoo.com>
2024-03-23 16:56:34 +0530

    established global context for the socket connection in client

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { SocketProvider } from "./context/SocketProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <SocketProvider>
+        <App />
+      </SocketProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
